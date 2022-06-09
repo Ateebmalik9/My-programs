0a65f8ca1b97989b3c36ad075139718f68a657a9
Ateeb Ahmed Malik <68679919+Ateebmalik9@users.noreply.github.com>
2022-06-09 19:29:11 +0530

    Create even_odd.c

---
+++ even_odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf("Enter any number: ");
+ scanf("%d", &a);
+ if(a%2==0)
+ {
+  printf("Entered number is Even");
+ }
+ else
+ {
+  printf("Entered number is Odd");
+ }
+ return 0;
+}
+ 
