cd4adc8d2e40f4cb9ef0b3b24b7ad4b0a59a2fdc
Ateeb Ahmed Malik <68679919+Ateebmalik9@users.noreply.github.com>
2022-06-25 12:23:21 +0530

    Create increment decrement.c

---
+++ increment decrement.c
@@ -0,0 +1,22 @@
+// i++ , --i
+#include <stdio.h>
+
+int main() {
+    int i=0,j=5;
+    
+    printf("%d\n",i);
+    printf("%d\n",++i);
+    printf("%d\n",i++);
+    printf("%d\n",i);
+    
+    printf("\n");
+    printf("\n");
+    
+    printf("%d\n",j);
+    printf("%d\n",--j);
+    printf("%d\n",j--);
+    printf("%d\n",j);
+
+
+    return 0;
+}
