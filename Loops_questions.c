78ccb14b4379b57a53048e256987b82d06161b76
Ateeb Ahmed Malik <68679919+Ateebmalik9@users.noreply.github.com>
2022-06-26 11:28:17 +0530

    Update Loops_questions.c

--- Loops_questions.c
+++ Loops_questions.c
@@ -134,7 +134,6 @@ int main() {
         j=2*i-1;
         sum=sum+j;
         printf("%d ",j);
-       // sum=sum+i+2;
     } 
     
     printf("\nThe sum of first %d odd natural numbers is %d: ",n,sum);
@@ -230,3 +229,226 @@ int main() {
 }
 
 Q12
+#include <stdio.h>
+
+int main() {
+    int i,j,k=1;
+    
+
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=i;j++)
+     {
+        printf("%d",k++); //k++ means post increment
+     }
+     printf("\n");
+    } 
+    
+    return 0;
+}
+
+Q13
+
+#include <stdio.h>
+
+int main() {
+    int i,j,k,l=0;
+    
+
+    for(i=1;i<=4;i++)
+    {
+      for(j=4;j>=i;j--)
+      {
+          printf(" ");
+      }
+      
+      for(k=1;k<=i;k++)
+      {
+          printf("%d ",++l); // pre increment
+      }
+     printf("\n");
+    } 
+    
+    return 0;
+}
+
+Q14
+#include <stdio.h>
+
+int main() {
+    int i,j,k,l=0;
+    
+
+    for(i=1;i<=4;i++)
+    {
+      for(j=4;j>=i;j--)
+      {
+          printf(" ");
+      }
+      
+      for(k=1;k<=i;k++) 
+      {
+          printf("* "); 
+      }
+     printf("\n");
+    } 
+    
+    return 0;
+}
+
+Q15
+#include <stdio.h>
+
+int main() {
+    int i,n,fact=1;
+    
+    printf("Enter any number: ");
+    scanf("%d",&n);
+
+    for(i=n;i>=1;i--)
+    {
+        fact=fact*i;
+    }
+    printf("The factorial of %d is: %d",n,fact);
+    return 0;
+}
+
+Q16
+#include <stdio.h>
+
+int main() {
+    int i,j, n, sum=0;
+    
+    printf("Number of first even natural numbers to be added: ");
+    scanf("%d",&n);
+    printf("The odd numbers are: ");
+    for(i=1;i<=n;i++)
+    {
+        j=2*i;
+        sum=sum+j;
+        printf("%d ",j);
+       
+    } 
+    
+    printf("\nThe sum of first %d even natural numbers is %d: ",n,sum);
+    
+    return 0;
+}
+
+Q17
+#include <stdio.h>
+
+int main() {
+    int i,j,k;
+    
+
+    for(i=1;i<=4;i++)
+    {
+      for(j=4;j>=i;j--)
+      {
+          printf(" ");
+      }
+      
+      for(k=1;k<=i;k++)
+      {
+           if(i==1)
+        {
+            printf("1 ");
+        }
+        else if(i==2)
+        {
+            printf("2 ");
+        }
+        else if(i==3)
+        {
+            printf("3 ");
+        }
+        else
+        {
+            printf("4 ");
+        } 
+      }
+     printf("\n");
+    } 
+    
+    return 0;
+}
+
+Q18
+
+
+Q19
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    float  sum=0;
+    
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        printf("1/%d + ",i);
+        sum=sum+(float)1/i;
+     
+    } 
+    printf("\n%f", sum);
+    return 0;
+}
+
+Q20
+#include <stdio.h>
+
+int main() {
+    int i,j,k;
+    
+    
+    for(i=1;i<=3;i++)
+    {
+        for(j=3;j>i;j--)
+        {
+            printf(" ");
+        }
+       for(k=1;k<=2*i-1;k++)
+       {
+           printf("*");
+       }
+        printf("\n");   
+    } 
+    
+    
+    return 0;
+}
+
+Q21
+
+Q22
+#include <stdio.h>
+
+int main() {
+    int i,j,k;
+    
+    
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            k=i+j;
+            if(k%2==0)
+            {
+                printf("1");
+            }
+            else
+            {
+                printf("0");
+            }
+        }
+      
+        printf("\n");
+    } 
+    
+    
+    
+    return 0;
+}
