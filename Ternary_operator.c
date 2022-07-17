0914ffe3b715a7faa7f31a46830381802afdc41c
Ateeb Ahmed Malik <68679919+Ateebmalik9@users.noreply.github.com>
2022-07-17 16:37:59 +0530

    Update Ternary_operator.c

--- Ternary_operator.c
+++ Ternary_operator.c
@@ -1,4 +1,15 @@
 // C program to use ternary operator
+
+/*
+Synatx of Ternary operator
+testCondition ? expression1 : expression 2;
+
+The testCondition is a boolean expression that results in either true or false. If the condition is
+
+true - expression1 (before the colon) is executed
+false - expression2 (after the colon) is executed
+
+*/
 #include <stdio.h>
 
 int main() 
